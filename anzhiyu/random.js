0ce32b957711286463f13fd4f4f7298e3ba5e652
DQY0909 <209733813@qq.com>
2024-08-01 00:23:15 +0800

    Site updated: 2024-08-01 00:23:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/06/hexo博客文章页面加密/","2023/10/06/hexo博客butterfly主题图库创建/"];function toRandomPost(){
+var posts=["2023/10/06/hexo博客文章页面加密/","2023/10/06/hexo博客butterfly主题图库创建/","2024/07/30/安知鱼主题中的关于本人界面的生涯模块魔改成恋爱计时/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
